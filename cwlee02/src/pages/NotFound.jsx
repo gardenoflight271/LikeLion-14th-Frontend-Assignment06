f737d1f8ab497bde55e1e73586302a148ce1c408
Lee Chaewon <hyr.chaewon@gmail.com>
2026-05-30 23:20:34 +0900

    제출

--- cwlee02/src/pages/NotFound.jsx
+++ cwlee02/src/pages/NotFound.jsx
@@ -2,9 +2,10 @@ import { NavLink } from "react-router-dom"
 
 export default function NotFound() {
     return (
-        <div>
-            <h1>404</h1>
-            <NavLink to="/">홈으로 이동</NavLink >
+        <div className="flex flex-col items-center">
+            <h1 className="text-red-500 text-6xl h-12 flex items-center justify-center mb-6 p-20">404 error</h1>
+            <NavLink className="bg-slate-300 p-1 px-4 rounded-md items-end"
+                to="/">홈으로 이동</NavLink >
         </div>
     )
 }
